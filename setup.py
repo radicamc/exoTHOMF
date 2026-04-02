20929b1cb5ea873fbc3d3e06f742ef1ff5ed1ca4
radicamc <radicamc@uchicago.edu>
2026-04-02 10:52:50 -0500

    Update readme

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 
 from setuptools import setup
 
-setup(name='stellarfit',
-      version='0.0.1',
+setup(name='exothomf',
+      version='1.0.0',
       license='MIT',
       author='Michael Radica',
       author_email='radicamc@uchicago.edu',
-      packages=['stellarfit'],
+      packages=['exothomf'],
       include_package_data=True,
-      url='https://github.com/radicamc/StellarFit',
-      description='Tools for Stellar Spectrum Fitting',
+      url='https://github.com/radicamc/exoTHOMF',
+      description='Tools for Stellar Spectrum and Contamination Fitting',
       package_data={'': ['README.md', 'LICENSE']},
       install_requires=['astropy', 'corner', 'dynesty', 'emcee', 'h5py', 'matplotlib',
                         'numpy==1.24.4', 'pandas', 'requests', 'scipy', 'spectres', 'tqdm'],
